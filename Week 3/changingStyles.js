3ba273802ac0b36782e402463afacb90499f60d9
Jbhupp <103956990+Jbhupp@users.noreply.github.com>
2022-06-29 21:18:59 -0400

    commit

--- Week 3/changingStyles.js
+++ Week 3/changingStyles.js
@@ -1,14 +1,27 @@
 var h = document.createElement("H2");  
 var t = document.createTextNode("Welcome"); 
 h.appendChild(t); 
-
 document.body.appendChild(h); 
 
 for (let i = 0; i < 4; i ++) {
     var ele = document.createElement("h2");
     ele.innerHTML = "Welcome";
     document.body.appendChild(ele);
+
+
     
 }
-var heading1 = document.getElementById("h2")
-heading1.style.color ="cornflowerblue"
+
+for (let i = 0; i < 10; i ++){
+    var note = document.querySelector('H2');
+    note.style.backgroundColor = 'cornflowerblue';
+    note.style.cssText += 'font-size:20px; font-weight: lighter; font-family: sans-serif'
+    document.body.appendChild(note)
+}
+
+
+
+const heading = document.createElement('heading')
+heading.classList.add("border")
+
+
