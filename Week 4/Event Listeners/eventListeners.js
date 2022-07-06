5f9c28c93475906832aba3c4366a6b6b6561a0c2
Jbhupp <103956990+Jbhupp@users.noreply.github.com>
2022-07-05 21:02:10 -0400

    commit

---
+++ Week 4/Event Listeners/eventListeners.js
@@ -0,0 +1,45 @@
+const square = document.getElementById("square")
+
+function handleLoad(){
+    square.style.backgroundColor ="black";
+}
+
+square.addEventListener("load", handleLoad)
+
+
+function handleStart(){
+    square.style.backgroundColor ="white";
+}
+
+square.addEventListener("mouseover", handleStart)
+
+
+function handleMouseOver(){
+    square.style.backgroundColor ="green";
+}
+
+square.addEventListener("mouseover", handleMouseOver)
+
+function handleMouseDown(){
+    square.style.backgroundColor ="yellow";
+}
+
+square.addEventListener("mousedown", handleMouseDown)
+
+function handleMouseUp(){
+    square.style.backgroundColor ="blue";
+}
+
+square.addEventListener("mouseup", handleMouseUp)
+
+function handleMouseDouble(){
+    square.style.backgroundColor ="red";
+}
+
+square.addEventListener("dblclick", handleMouseDouble)
+
+function handleMouseScroll(){
+    square.style.backgroundColor ="purple";
+}
+
+square.addEventListener("scroll", handleMouseScroll)
